6eaa1d146f13e1b2f8d5910f2642303415e6c54d
Mathieu Clerici <mathieu.clerici@gmail.com>
2020-11-04 19:46:19 +0300

    [FEATURE] - Added support for numeric type in custo profile fields.

--- client/components/CustomFieldsForm.js
+++ client/components/CustomFieldsForm.js
@@ -18,6 +18,38 @@ const CustomNumericInput = ({ name, required, setState, state, className }) => {
 		}
 
 		return errors.join(', ');
+	}, [state, required, minLength, t]);
+
+	const validateNumericInput = (e) => {
+		let value = e.target.value;
+
+        if (!Number(value)) {
+            return;
+		}
+
+		setState(e.currentTarget.value);
+	}
+
+	return useMemo(() => <Field className={className}>
+		<Field.Label>{t(name)}</Field.Label>
+		<Field.Row>
+			<TextInput name={name} error={verify} flexGrow={1} value={state} required={required} onChange={validateNumericInput}/>
+		</Field.Row>
+		<Field.Error>{verify}</Field.Error>
+	</Field>, [className, t, name, verify, maxLength, state, required, setState]);
+};
+
+const CustomNumericInput = ({ name, required, setState, state, className }) => {
+	const t = useTranslation();
+
+	const verify = useMemo(() => {
+		const errors = [];
+
+		if (!state && required) {
+			errors.push(t('Field_required'));
+		}
+
+		return errors.join(', ');
 	}, [state, required, t]);
 
 	const validateNumericInput = (e) => {
