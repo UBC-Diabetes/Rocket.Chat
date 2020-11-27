6e1535ecdf59d140185666f7ec15fa2aab56d647
Mathieu Clerici <mathieu.clerici@gmail.com>
2020-11-27 22:39:56 +0300

    [FEATURE] - Added necessary endpoint to fetch peer supporters only.

--- server/main.js
+++ server/main.js
@@ -23,6 +23,7 @@ import './methods/addRoomModerator';
 import './methods/addRoomOwner';
 import './methods/afterVerifyEmail';
 import './methods/browseChannels';
+import './methods/browseProfileLibrary';
 import './methods/canAccessRoom';
 import './methods/channelsList';
 import './methods/createDirectMessage';
