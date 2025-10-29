a2bd8dbf954a5ecf45875fbb157d2254d9124193
Timothy Roy <timothy.roy@pm.me>
2025-10-29 17:10:50 -0500

    Try individual DMs with tech support

--- app/lib/server/functions/techSupportDuplication.js
+++ app/lib/server/functions/techSupportDuplication.js
@@ -1,8 +1,19 @@
 import { callbacks } from '../../../callbacks/server';
-import { Messages, Rooms, Users, Subscriptions } from '../../../models/server';
+import { Messages, Rooms, Users } from '../../../models/server';
+import { createDirectMessage } from '../../../../server/methods/createDirectMessage';
+import { Logger } from '../../../logger/server';
+
+const logger = new Logger('tech-support-duplication');
+
+
+// Define the list of administrators who should see tech_support messages
+const TECH_SUPPORT_ADMINS = [
+	'timq',
+	// Add more usernames as needed
+];
 
 /**
- * Duplicates messages sent TO tech_support user into #tech_support_feed channel
+ * Duplicates messages sent TO tech_support user into DMs with each admin
  */
 function duplicateTechSupportMessages(message, room) {
 	// Only process direct messages
@@ -25,36 +36,67 @@ function duplicateTechSupportMessages(message, room) {
 		return message;
 	}
 
-	// Find the tech_support_feed channel
-	const targetRoom = Rooms.findOneByName('tech_support_feed');
-	if (!targetRoom) {
-		return message;
-	}
-
 	// Get tech_support user
 	const techSupportUser = Users.findOneByUsername('tech_support');
 	if (!techSupportUser) {
 		return message;
 	}
 
-	try {
-		// Create duplicate message posted by tech_support
-		Messages.insert({
-			rid: targetRoom._id,
-			ts: new Date(),
-			msg: `**Message from @${message.u.username}:**\n${message.msg}`,
-			u: {
-				_id: techSupportUser._id,
-				username: techSupportUser.username,
-				name: techSupportUser.name || techSupportUser.username
-			},
-			mentions: [],
-			channels: [],
-			_updatedAt: new Date()
-		});
-	} catch (error) {
-		console.error('[TechSupport] Error duplicating message:', error);
-	}
+	// Process each admin
+	TECH_SUPPORT_ADMINS.forEach(adminUsername => {
+		try {
+			const adminUser = Users.findOneByUsername(adminUsername);
+			if (!adminUser) {
+				console.log(`[TechSupport] Admin user ${adminUsername} not found`);
+				return;
+			}
+
+			// Find or create DM between admin and tech_support
+			let adminTechRoom = Rooms.findOneDirectRoomContainingAllUserIDs([adminUser._id, techSupportUser._id]);
+			
+			if (!adminTechRoom) {
+				// Create DM between admin and tech_support
+				const roomResult = createDirectMessage([techSupportUser.username], adminUser._id);
+				if (roomResult && roomResult.rid) {
+					adminTechRoom = Rooms.findOneById(roomResult.rid);
+				}
+			}
+
+			if (!adminTechRoom) {
+				console.log(`[TechSupport] Could not find/create DM between ${adminUsername} and tech_support`);
+				return;
+			}
+
+            logger.debug(`Duplicating message from ${message.u.username} to admin ${adminUsername}`);
+
+			// Create duplicate message posted by tech_support in the admin DM
+			Messages.insert({
+				rid: adminTechRoom._id,
+				ts: new Date(),
+				msg: `**Message from @${message.u.username}:**\n${message.msg}`,
+				u: {
+					_id: techSupportUser._id,
+					username: techSupportUser.username,
+					name: techSupportUser.name || techSupportUser.username
+				},
+				mentions: [],
+				channels: [],
+				_updatedAt: new Date(),
+				customFields: {
+					duplicatedFrom: {
+						originalSender: message.u.username,
+						originalMessageId: message._id,
+						originalRoom: room._id
+					}
+				}
+			});
+
+			console.log(`[TechSupport] Duplicated message from ${message.u.username} to admin ${adminUsername}`);
+
+		} catch (error) {
+			console.error(`[TechSupport] Error duplicating message to admin ${adminUsername}:`, error);
+		}
+	});
 
 	return message;
 }
